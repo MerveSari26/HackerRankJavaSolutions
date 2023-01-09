7d1ebe1efd4b381dc7e623aea83c0cea96f10174
unknown <mervekati350@gmail.com>
2023-01-10 02:11:10 +0300

    Java Datatypes Solution

---
+++ HackerRank_Java_Datatypes_Solution.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int T=sc.nextInt();
+       long number;
+       for(int i=0;i<T;i++){
+           try{
+            number=sc.nextLong();
+            System.out.println(number+" can be fitted in:");
+            if(number>=Byte.MIN_VALUE && number<=Byte.MAX_VALUE){
+                System.out.println("* byte");
+                System.out.println("* short");
+                System.out.println("* int");
+                System.out.println("* long");
+                continue;
+            }
+            else if(number>=Short.MIN_VALUE && number<=Short.MAX_VALUE){
+                System.out.println("* short");
+                System.out.println("* int");
+                System.out.println("* long");
+                continue;
+            }
+            else if(number>=Integer.MIN_VALUE && number<=Integer.MAX_VALUE){
+                System.out.println("* int");
+                System.out.println("* long");
+                continue;
+            }
+            else
+                System.out.println("* long");
+           }
+           catch(Exception e){
+               System.out.println(sc.next()+" can't be fitted anywhere.");
+           }
+       }
+    }
+}
