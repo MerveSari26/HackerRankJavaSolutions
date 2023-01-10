1ecb6f9f55d25d2b2f0e0434881fb079d3e5ee69
unknown <mervekati350@gmail.com>
2023-01-10 03:58:08 +0300

    Java Datatypes Solution

--- HackerRank_Java_Datatypes_Solution.java
+++ HackerRank_Java_Datatypes_Solution.java
@@ -11,25 +11,12 @@ public class Solution {
            try{
             number=sc.nextLong();
             System.out.println(number+" can be fitted in:");
-            if(number>=Byte.MIN_VALUE && number<=Byte.MAX_VALUE){
+            if(number>=Byte.MIN_VALUE && number<=Byte.MAX_VALUE)
                 System.out.println("* byte");
+            if(number>=Short.MIN_VALUE && number<=Short.MAX_VALUE)
                 System.out.println("* short");
+            if(number>=Integer.MIN_VALUE && number<=Integer.MAX_VALUE)
                 System.out.println("* int");
-                System.out.println("* long");
-                continue;
-            }
-            else if(number>=Short.MIN_VALUE && number<=Short.MAX_VALUE){
-                System.out.println("* short");
-                System.out.println("* int");
-                System.out.println("* long");
-                continue;
-            }
-            else if(number>=Integer.MIN_VALUE && number<=Integer.MAX_VALUE){
-                System.out.println("* int");
-                System.out.println("* long");
-                continue;
-            }
-            else
                 System.out.println("* long");
            }
            catch(Exception e){
